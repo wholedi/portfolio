39fdc87f92fea4b1c3e3bf3c3ddcc34dc748f76f
Diana <131402561+wholedi@users.noreply.github.com>
2024-02-01 19:46:17 +0200

    Add files via upload

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -2,3 +2,11 @@ const icon = document.querySelector('.icon-menu');
 icon.addEventListener('click', function () {
     document.documentElement.classList.toggle('menu-open');
 })
+
+const menuLinks = document.querySelectorAll('.menu__link');
+
+menuLinks.forEach(function(menuLink) {
+    menuLink.addEventListener('click', function() {
+        document.documentElement.classList.remove('menu-open');
+    });
+});
